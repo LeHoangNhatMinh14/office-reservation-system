192a3c7c59e69c35be556a5bc963cb71085bc6e8
Miguel Werneck <mig.roale@gmail.com>
2024-11-01 21:13:50 +0100

    disable auth

--- office-reservation-api/src/main/java/nl/fontys/s3/officereservationsystem/OfficeReservationSystemApplication.java
+++ office-reservation-api/src/main/java/nl/fontys/s3/officereservationsystem/OfficeReservationSystemApplication.java
@@ -2,6 +2,12 @@ package nl.fontys.s3.officereservationsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class OfficeReservationSystemApplication {
@@ -10,4 +16,14 @@ public class OfficeReservationSystemApplication {
         SpringApplication.run(OfficeReservationSystemApplication.class, args);
     }
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+    }
 }
