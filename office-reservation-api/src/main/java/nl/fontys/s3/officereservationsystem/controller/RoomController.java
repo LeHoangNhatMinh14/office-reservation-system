cfcd6becd4e25b84f8dcd8637116c1b92b6a635c
Minh <minh.le@student.fontys.nl>
2024-11-03 18:47:26 +0100

    small change to the api end point name

--- office-reservation-api/src/main/java/nl/fontys/s3/officereservationsystem/controller/RoomController.java
+++ office-reservation-api/src/main/java/nl/fontys/s3/officereservationsystem/controller/RoomController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/rooms")
+@RequestMapping("/rooms")
 @AllArgsConstructor
 public class RoomController {
 
