c38e63411ecfad97328a9d2a82a73e82ad782a0e
Francisco Sanchez <i501613@fontys.student.nl>
2024-11-13 13:17:05 +0100

    Flyway migration for reservation table

---
+++ office-reservation-api/src/main/resources/db/migration/V2__CreateReservationTable.sql
@@ -0,0 +1,16 @@
+-- Create reservation table
+CREATE TABLE reservation (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    date DATE NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    reservation_type VARCHAR(50) NOT NULL,
+    room_id BIGINT NOT NULL,
+    table_id BIGINT NOT NULL,
+    seated_user_id BIGINT NOT NULL,
+    reservation_user_id BIGINT NOT NULL,
+    FOREIGN KEY (room_id) REFERENCES room(id) ON DELETE CASCADE,
+    FOREIGN KEY (table_id) REFERENCES room_table(id) ON DELETE CASCADE,
+    FOREIGN KEY (seated_user_id) REFERENCES user(id) ON DELETE CASCADE,
+    FOREIGN KEY (reservation_user_id) REFERENCES user(id) ON DELETE CASCADE
+);
