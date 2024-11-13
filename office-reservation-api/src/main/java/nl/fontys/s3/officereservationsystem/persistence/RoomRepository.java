cd7023193bef2382eeec3bd66e89721415640cd7
Francisco Sanchez <i501613@fontys.student.nl>
2024-11-13 21:19:04 +0100

    Initialize dummy data

--- office-reservation-api/src/main/java/nl/fontys/s3/officereservationsystem/persistence/RoomRepository.java
+++ office-reservation-api/src/main/java/nl/fontys/s3/officereservationsystem/persistence/RoomRepository.java
@@ -3,5 +3,8 @@ package nl.fontys.s3.officereservationsystem.persistence;
 import nl.fontys.s3.officereservationsystem.persistence.entity.RoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoomRepository extends JpaRepository<RoomEntity, Long> {
+    Optional<RoomEntity> findByName(String name);
 }
--- office-reservation-api/src/main/resources/db/migration/V2__CreateReservationTable.sql
+++ office-reservation-api/src/main/resources/db/migration/V2__CreateReservationTable.sql
@@ -5,12 +5,10 @@ CREATE TABLE reservation (
     start_time TIME NOT NULL,
     end_time TIME NOT NULL,
     reservation_type VARCHAR(50) NOT NULL,
-    room_id BIGINT NOT NULL,
     table_id BIGINT NOT NULL,
     seated_user_id BIGINT NOT NULL,
     reservation_user_id BIGINT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (room_id) REFERENCES room(id) ON DELETE CASCADE,
     FOREIGN KEY (table_id) REFERENCES room_table(id) ON DELETE CASCADE,
     FOREIGN KEY (seated_user_id) REFERENCES user(id) ON DELETE CASCADE,
     FOREIGN KEY (reservation_user_id) REFERENCES user(id) ON DELETE CASCADE
