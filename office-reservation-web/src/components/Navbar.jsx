ceb146db54e53f9752897f08a0c96a4e2c881cae
José Gonçalves Pestana <i506501@FontysICT.nl>
2024-11-15 23:41:10 +0100

    NavBar fix

--- office-reservation-web/src/components/Navbar.jsx
+++ office-reservation-web/src/components/Navbar.jsx
@@ -6,12 +6,6 @@ function Navbar() {
     return (
         <nav className={styles.navBar}>
             <h1 className={styles.logo}>driessen</h1>
-            <ul className={styles.navLinks}>
-                <li><Link to="/room">Room</Link></li>
-                <li><Link to="/schedule">Schedule</Link></li>
-                <li><Link to="/teams">Teams</Link></li>
-                <li><Link to="/reservation">Reservation</Link></li>
-            </ul>
         </nav>
     );
 }
